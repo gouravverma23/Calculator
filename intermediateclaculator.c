71e9e2c8801f80c4378618b62ad422c6b7061620
gouravverma23 <gauravverma@23042006@gmail.com>
2024-11-11 23:51:04 +0530

    add factorial

--- intermediateclaculator.c
+++ intermediateclaculator.c
@@ -6,11 +6,12 @@ char addition(int*a){
         scanf("%d",&b);
         printf("%d\n",*a+b);
          *a=*a+b;
-    printf("Enter further operation (+,-,*,/) :- ");
+    printf("Enter further operation (+,-,*,/,!) :- ");
     scanf(" %c",&x);
     }
     return x;
 }
+
 char subtraction(int*a){
     int b; char x='-';
     while(x=='-'){
@@ -18,11 +19,12 @@ char subtraction(int*a){
         scanf("%d",&b);
         printf("%d\n",*a-b);
          *a=*a-b;
-    printf("Enter further operation (+,-,*,/) :- ");
+    printf("Enter further operation (+,-,*,/,!) :- ");
     scanf(" %c",&x);
     }
     return x;
 }
+
 char multiplication(int*a){
     int b; char x='*';
     while(x=='*'){
@@ -30,11 +32,12 @@ char multiplication(int*a){
         scanf("%d",&b);
         printf("%d\n",*a*b);
          *a=*a*b;
-    printf("Enter further operation (+,-,*,/) :- ");
+    printf("Enter further operation (+,-,*,/,!) :- ");
     scanf(" %c",&x);
     }
     return x;
 }
+
 char division(int*a){
     int b; char x='/';
     while(x=='/'){
@@ -42,22 +45,39 @@ char division(int*a){
         scanf("%d",&b);
         printf("%d\n",*a/b);
          *a=*a/b;
-    printf("Enter further operation (+,-,*,/) :- ");
+    printf("Enter further operation (+,-,*,/,!) :- ");
     scanf(" %c",&x);
     }
     return x;
 }
+
+char factorial(int*a){
+    char x='!';
+    while(x=='!'){
+        int fact=1;
+        for(int i=1;i<=*a;i++){
+            fact=fact*i;
+        }
+        printf("%d\n",fact);
+        *a=fact;
+    printf("Enter further operation (+,-,*,/,!) :- ");
+    scanf(" %c",&x);
+    }
+    return x;
+}
+
 int main(){
     int a; char x;
     printf("Enter the first number: ");
     scanf("%d",&a);
-    printf("What do you want to do +,-,*,/ : ");
+    printf("What do you want to do +,-,*,/,!(for factorial) : ");
     scanf(" %c",&x);
-    while(x=='+' || x=='-' || x=='*' || x=='/'){
+    while(x=='+' || x=='-' || x=='*' || x=='/' || x=='!'){
         if(x=='+') x=addition(&a);
         if(x=='-') x=subtraction(&a);
         if(x=='*') x=multiplication(&a);
         if(x=='/') x=division(&a);
+        if(x=='!') x=factorial(&a);
     }
     return 0;
 }
