78bd78cae67748510dac6c1e0e244432eb593797
gouravverma23 <gauravverma@23042006@gmail.com>
2024-11-11 23:12:06 +0530

    add some new feature like addition, subtractio, multiplication and division all in one series of run

--- intermediateclaculator.c
+++ intermediateclaculator.c
@@ -1,93 +1,63 @@
 #include<stdio.h>
-void addition2(int a,int b){
-    
-    printf("The sum is equal to = %d\n",a+b);
-    return;
-}
-void addition(int a){
-    int b;
-    char x='+';
+char addition(int*a){
+    int b; char x='+';
     while(x=='+'){
         printf("Enter the next number :- ");
-    scanf("%d",&b);
-        addition2(a,b);
-         a=a+b;
-    printf("Enter + if you want to add further or just space and enter if you want to close :- ");
+        scanf("%d",&b);
+        printf("%d\n",*a+b);
+         *a=*a+b;
+    printf("Enter further operation (+,-,*,/) :- ");
     scanf(" %c",&x);
     }
-    return;
+    return x;
 }
-void subtraction2(int a,int b){
-    
-    printf("The sum is equal to = %d\n",a-b);
-    return;
-}
-void subtraction(int a){
-    int b;
-    char x='-';
+char subtraction(int*a){
+    int b; char x='-';
     while(x=='-'){
         printf("Enter the next number :- ");
-    scanf("%d",&b);
-        subtraction2(a,b);
-         a=a-b;
-    printf("Enter - if you want to subtract further or just space and enter if you want to close :- ");
+        scanf("%d",&b);
+        printf("%d\n",*a-b);
+         *a=*a-b;
+    printf("Enter further operation (+,-,*,/) :- ");
     scanf(" %c",&x);
     }
-    return;
+    return x;
 }
-void multiplication2(int a,int b){
-    
-    printf("The sum is equal to = %d\n",a*b);
-    return;
-}
-void multiplication(int a){
-    int b;
-    char x='*';
+char multiplication(int*a){
+    int b; char x='*';
     while(x=='*'){
         printf("Enter the next number :- ");
-    scanf("%d",&b);
-        multiplication2(a,b);
-         a=a*b;
-    printf("Enter * if you want to multiply further or just space and enter if you want to close :- ");
+        scanf("%d",&b);
+        printf("%d\n",*a*b);
+         *a=*a*b;
+    printf("Enter further operation (+,-,*,/) :- ");
     scanf(" %c",&x);
     }
-    return;
+    return x;
 }
-void division2(int a,int b){
-    
-    printf("The sum is equal to = %d\n",a/b);
-    return;
-}
-void division(int a){
-    int b;
-    char x='/';
+char division(int*a){
+    int b; char x='/';
     while(x=='/'){
         printf("Enter the next number :- ");
-    scanf("%d",&b);
-        division2(a,b);
-         a=a/b;
-    printf("Enter / if you want to divide further or just space and enter if you want to close :- ");
+        scanf("%d",&b);
+        printf("%d\n",*a/b);
+         *a=*a/b;
+    printf("Enter further operation (+,-,*,/) :- ");
     scanf(" %c",&x);
     }
-    return;
+    return x;
 }
 int main(){
     int a; char x;
-    printf("Enter the first number :- ");
+    printf("Enter the first number: ");
     scanf("%d",&a);
-    printf("Enter + for addition, - for subtraction, * for multiplication, / for division :- ");
+    printf("What do you want to do +,-,*,/ : ");
     scanf(" %c",&x);
-    if(x=='+'){
-        addition(a);
-    }
-    if(x=='-'){
-        subtraction(a);
-    }
-    if(x=='*'){
-        multiplication(a);
-    }
-    if(x=='/'){
-        division(a);
+    while(x=='+' || x=='-' || x=='*' || x=='/'){
+        if(x=='+') x=addition(&a);
+        if(x=='-') x=subtraction(&a);
+        if(x=='*') x=multiplication(&a);
+        if(x=='/') x=division(&a);
     }
     return 0;
 }
